93e5e9d2b8ff03ace3f4b2e90753b78d41b5a274
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2023-12-11 17:07:52 -0500

    create TagManager class to handle tag input logic

--- GeneralBlogApp/wwwroot/js/site.js
+++ GeneralBlogApp/wwwroot/js/site.js
@@ -1,55 +1,52 @@
-﻿const ul = document.querySelector(".tag-box ul");
-const input = ul.querySelector("input");
-const countNumb = document.querySelector(".details span");
-let maxTags = 10,
-tags = [];
+﻿class TagManager {
+    constructor(ulSelector, inputSelector, countNumbSelector, maxTags) {
+        this.ul = document.querySelector(ulSelector);
+        this.input = this.ul.querySelector(inputSelector);
+/*        this.countNumb = document.querySelector(countNumbSelector);*/
+        this.maxTags = maxTags || 10;
+        this.tags = [];
 
-//countTags();
-//function countTags() {
-//    countNumb.innerText = maxTags - tags.length;
-//}
-function createTag() {
-    ul.querySelectorAll("li").forEach(li => li.remove());
-    tags.slice().reverse().forEach(tag => {
-        let liTag = `<li>${tag}<i class="fa-solid fa-xmark" onclick="removeTag(this, '${tag}')"></i></li>`;
-        ul.insertAdjacentHTML("afterbegin", liTag);
-    })
-    /*countTags();*/
-}
-
-function removeTag(elem, tag) {
-    let index = tags.indexOf(tag);
-    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
-    elem.parentElement.remove();
-   /* countTags();*/
-}
-function addTag(e) {
-    if (e.key == "Enter") {
-        e.preventDefault();
-        let tag = e.target.value.replace(/\s+/g, ' ');
-        if (tag.length > 1 && !tags.includes(tag)) {
-            tag.split(",").forEach(tag => {
-                tags.push(tag);
-                createTag();
-            });
-        }
-        e.target.value = "";
+        // Initialize the tag manager
+        this.init();
     }
 
-    
-}
-$(document).on("keydown", "form", function (event) {
-    return event.key != "Enter";
-});
-input.addEventListener("keyup", addTag);
-$('#createCatForm').submit(function (event) {
-    // Add hidden input to store tags array in the form data
-    $('<input>').attr({
-        type: 'hidden',
-        name: 'MetaKeywords',
-        value: JSON.stringify(tags)
-    }).appendTo('#createCatForm');
+    init() {
+        this.input.addEventListener("keyup", this.addTag.bind(this));
+        // Add any additional initialization logic here
+    }
 
-    // Uncomment the line below to prevent the form from actually submitting
-    // event.preventDefault();
-});
+    createTag() {
+        this.ul.querySelectorAll("li").forEach(li => li.remove());
+        this.tags.slice().reverse().forEach(tag => {
+            let liTag = `<li>${tag}<i class="fa-solid fa-xmark" onclick="removeTag(this, '${tag}')"></i></li>`;
+            this.ul.insertAdjacentHTML("afterbegin", liTag);
+        });
+        /*this.countTags();*/
+    }
+
+    removeTag(elem, tag) {
+        let index = this.tags.indexOf(tag);
+        this.tags = [...this.tags.slice(0, index), ...this.tags.slice(index + 1)];
+        elem.parentElement.remove();
+       /* this.countTags();*/
+    }
+
+    addTag(e) {
+        if (e.key == "Enter") {
+            e.preventDefault();
+            let tag = e.target.value.replace(/\s+/g, ' ');
+            if (tag.length > 1 && !this.tags.includes(tag)) {
+                tag.split(",").forEach(tag => {
+                    this.tags.push(tag);
+                    this.createTag();
+                });
+            }
+            e.target.value = "";
+        }
+    }
+
+    //countTags() {
+    //    this.countNumb.innerText = this.maxTags - this.tags.length;
+    //}
+}
+
