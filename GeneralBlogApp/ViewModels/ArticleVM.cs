8ff0a05e168e9edce9e5abd76deda36603f97443
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2023-12-13 12:08:07 -0500

    Add ArticleVM

---
+++ GeneralBlogApp/ViewModels/ArticleVM.cs
@@ -0,0 +1,28 @@
+﻿using GeneralBlogApp.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace CLK_BlogAdmin.ViewModels
+{
+	public class ArticleVM
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required(ErrorMessage = "required", AllowEmptyStrings = false)]
+		public string Title { get; set; } = string.Empty;
+		[Required(ErrorMessage = "required", AllowEmptyStrings = false)]
+		public int CategoryId { get; set; }
+		public Article Article { get; set; }
+		public string? CategoryName { get; set; }
+		//public string? AuthorDisplayName { get; set; }
+		public IFormFile? MainImageFile { get; set; }
+		public string[]? MetaKeywords { get; set; }
+		//public bool? IsPublished { get; set; }
+		//public bool? IsArchived { get; set; }
+		[ValidateNever]
+		public IEnumerable<SelectListItem> CategoryList { get; set; } = Enumerable.Empty<SelectListItem>();
+
+
+	}
+}
